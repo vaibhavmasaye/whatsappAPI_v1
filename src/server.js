959e1eb3af04e592aa04e58c463ed9d59cc6bc03
vaibhavMasaye2000 <masayevaibhav@gmail.com>
2025-09-11 16:18:20 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const port = 3020;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
