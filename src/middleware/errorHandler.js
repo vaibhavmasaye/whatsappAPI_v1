5e29941d1f8890e3d19a5393e12c375092466801
VaibhavMasaye45 <47131806+vaibhavmasaye@users.noreply.github.com>
2025-09-11 15:49:54 +0530

    Delete src directory

--- src/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const logger = require('../logger');
-
-module.exports = (err, req, res, next) => {
-  logger.error('Unhandled error', { message: err.message, stack: err.stack });
-  res.status(500).json({ error: 'Internal server error' });
-};
